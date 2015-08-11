e7f3ffa789612a7e7c5db0b898d9fa3fbee4e851
Artit Kiuwilai <first087@gmail.com>
2015-08-11 09:53:21 +0700

    Custom binding class name (ex 3)

--- app/src/main/java/com/artitk/android_databinding_example/MainActivity.java
+++ app/src/main/java/com/artitk/android_databinding_example/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import com.artitk.MainActivityBinding;
+import com.artitk.android_databinding_example.MainActivityBinding;
 
 public class MainActivity extends AppCompatActivity {
 
