9aa7e5836aabf506b68b684ab0969ba3b51cf824
Artit Kiuwilai <first087@gmail.com>
2015-08-11 10:28:13 +0700

    Remove unuse method for DataBinding

--- app/src/main/java/com/artitk/android_databinding_example/MainActivity.java
+++ app/src/main/java/com/artitk/android_databinding_example/MainActivity.java
@@ -19,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         // Sample Data
