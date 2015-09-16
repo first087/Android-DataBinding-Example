133897a80f0bddec05386fbdbf6e445457ac4aec
Artit Kiuwilai <first087@gmail.com>
2015-09-16 13:06:51 +0700

    Add ObservableField example

--- app/src/main/java/com/artitk/android_databinding_example/Employee.java
+++ app/src/main/java/com/artitk/android_databinding_example/Employee.java
@@ -2,6 +2,7 @@ package com.artitk.android_databinding_example;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.databinding.ObservableField;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.CompoundButton;
@@ -11,14 +12,14 @@ import com.artitk.android_databinding_example.BR;
 public class Employee extends BaseObservable {
     private String name;
     private int age;
-    private GenderEnum gender;
+    public ObservableField<GenderEnum> gender = new ObservableField<>();
 
     public enum GenderEnum {UNKNOWN, MALE, FEMALE}
 
     public Employee(String name, int age, GenderEnum gender) {
         this.name = name;
         this.age = age;
-        this.gender = gender;
+        this.gender.set(gender);
     }
 
     public String getName() {
@@ -71,10 +72,6 @@ public class Employee extends BaseObservable {
 //        notifyChange(); Use this method when more property change
     }
 
-    public GenderEnum getGender() {
-        return gender;
-    }
-
     public CompoundButton.OnCheckedChangeListener getGenderListener() {
         return new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -83,20 +80,16 @@ public class Employee extends BaseObservable {
 
                 int id = buttonView.getId();
                 switch (id) {
-                    case R.id.rdo_unknown:  gender = GenderEnum.UNKNOWN;    break;
-                    case R.id.rdo_male:     gender = GenderEnum.MALE;       break;
-                    case R.id.rdo_female:   gender = GenderEnum.FEMALE;     break;
+                    case R.id.rdo_unknown:  gender.set(GenderEnum.UNKNOWN); break;
+                    case R.id.rdo_male:     gender.set(GenderEnum.MALE);    break;
+                    case R.id.rdo_female:   gender.set(GenderEnum.FEMALE);  break;
                 }
             }
         };
     }
 
-    public void setGender(GenderEnum gender) {
-        this.gender = gender;
-    }
-
     @Override
     public String toString() {
-        return "name = " + name + "\nage = " + age + "\ngender = " + gender;
+        return "name = " + name + "\nage = " + age + "\ngender = " + gender.get();
     }
 }
